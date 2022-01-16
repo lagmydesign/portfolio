5797e158bc5ba5eb9ea254021bd8e4cfbb893b8f
lagmydesign <96060496+lagmydesign@users.noreply.github.com>
2022-01-16 23:19:25 +0100

    Add files via upload

---
+++ js/2D.js
@@ -0,0 +1,6 @@
+document.querySelector(".proj2").addEventListener("click", project2);
+
+function project2() {
+    var sigma = 'https://www.instagram.com/p/CIGuwWSCGrL/';
+    window.open(sigma,'_blank');
+}
