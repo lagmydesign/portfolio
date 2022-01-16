da3aca3ee89225fd164497580d3a8e5fb2ae1191
lagmydesign <96060496+lagmydesign@users.noreply.github.com>
2022-01-16 23:17:33 +0100

    Delete js directory

--- js/2D.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".proj2").addEventListener("click", project2);
-
-function project2() {
-    var sigma = 'https://www.instagram.com/p/CIGuwWSCGrL/';
-    window.open(sigma,'_blank');
-}
